96d5cde3974f1ab44671e4750f4d496432b4050f
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-05-25 20:46:15 -0400

    Initial Commit

---
+++ MyTinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTinder
+//
+//  Created by Daniel Goncalves on 2015-05-25.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
